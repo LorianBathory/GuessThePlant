5f4b6f6353097db64021ea9ca7b384371a6d4c25
Alisa <157376396+LorianBathory@users.noreply.github.com>
2025-09-27 11:36:21 +0200

    Add difficulty mappings for quiz questions

---
+++ src/data/difficulties.js
@@ -0,0 +1,24 @@
+export const difficultyLevels = Object.freeze({
+  EASY: 'Easy',
+  MEDIUM: 'Medium',
+  HARD: 'Hard'
+});
+
+export const questionIdsByDifficulty = Object.freeze({
+  [difficultyLevels.EASY]: Object.freeze([6, 31, 33, 35, 51, 55]),
+  [difficultyLevels.MEDIUM]: Object.freeze([2, 30, 3, 54, 4, 5]),
+  [difficultyLevels.HARD]: Object.freeze([1, 32, 34, 50, 53, 52])
+});
+
+const difficultyByQuestionId = Object.freeze(
+  Object.entries(questionIdsByDifficulty).reduce((acc, [difficulty, ids]) => {
+    ids.forEach(id => {
+      acc[id] = difficulty;
+    });
+    return acc;
+  }, {})
+);
+
+export function getDifficultyByQuestionId(questionId) {
+  return difficultyByQuestionId[questionId] || null;
+}
