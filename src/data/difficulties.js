7dd7baee1d9cb64ec7a9b7508588de01051f1e38
Alisa <157376396+LorianBathory@users.noreply.github.com>
2025-09-27 16:44:36 +0200

    Update difficulties.js

--- src/data/difficulties.js
+++ src/data/difficulties.js
@@ -6,7 +6,7 @@ export const difficultyLevels = Object.freeze({
 
 export const questionIdsByDifficulty = Object.freeze({
   [difficultyLevels.EASY]: Object.freeze([6, 31, 33, 35, 51, 55]),
-  [difficultyLevels.MEDIUM]: Object.freeze([2, 30, 3, 54, 4, 5]),
+  [difficultyLevels.MEDIUM]: Object.freeze([2, 30, 3, 54, 4, 5, 69]),
   [difficultyLevels.HARD]: Object.freeze([1, 32, 34, 50, 53, 52])
 });
 
