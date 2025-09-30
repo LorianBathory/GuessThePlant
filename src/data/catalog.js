c0ffbb091fd999b90802695140dd439b9737473a
Alisa Sinelnikova <lorianbathory@gmail.com>
2025-09-30 09:43:48 +0200

    добавила несколько новых цветов

--- src/data/catalog.js
+++ src/data/catalog.js
@@ -10,20 +10,25 @@ const speciesCatalog = Object.freeze({
   4:  { images: ['p004', 'p044'], wrongAnswers: [17, 18, 19, 20] },
   5:  { images: ['p005', 'p043', 'p054', 'p095'], wrongAnswers: [20, 21, 31, 23, 24] }, //camellia
   6:  { images: ['p006'], wrongAnswers: [25, 26, 27, 28, 29] },
+    8: {images: ['p107', 'p108', 'p109', 'p110', 'p111', 'p112', 'p113']}, //gazania
   13: {images: ['p086', 'p087', 'p088'], wrongAnswers: [3]}, //Allium, need some extra wrong answers
+    16: {images: ['p105'], wrongAnswers: [4]}, //Fritillary, need some extra wrong answers
   17: { images: ['p007'] },
   19: { images: ['p008'] },
   21: {images: ['p076'], wrongAnswers: [5, 14, 62] }, //добавить вишню для вариантов ответа
   22: { images: ['p009'] },
-  26: { images: ['p010', 'p011', 'p100'], wrongAnswers: [33, 73, 77, 39] }, //Dahlia
+    23: { images: ['p106'] }, //gardenia
+    24: { images: ['p120', 'p121'] }, //magnolia
+    26: { images: ['p010', 'p011', 'p100'], wrongAnswers: [33, 73, 77, 39] }, //Dahlia
   27: { images: ['p012', 'p103', 'p104'], wrongAnswers: [6, 26] },
   29: { images: ['p013', 'p014', 'p096', 'p097', 'p098'], wrongAnswers: [7, 41, 42] }, //Chrysanthemum
   30: { images: ['p015', 'p066'], wrongAnswers: [38, 39, 40, 6, 25, 16] }, //osteospermum
   31: { images: ['p016', 'p017', 'p018', 'p077', 'p078'], wrongAnswers: [7, 41, 42] },
   32: { images: ['p019'], wrongAnswers: [43, 44, 45] },
-  33: { images: ['p020', 'p021'], wrongAnswers: [31, 47, 46] },
+  33: { images: ['p020', 'p021', 'p122', 'p123'], wrongAnswers: [31, 47, 46] }, //Papaver
   34: { images: ['p022'], wrongAnswers: [43, 48, 49] },
-  35: { images: ['p023'], wrongAnswers: [16, 36, 37] },
+  35: { images: ['p023', 'p118', 'p119'], wrongAnswers: [16, 36, 37] },
+    36: { images: ['p114', 'p115'] }, //Hemerocallis
   41: { images: ['p024', 'p101'], wrongAnswers: [39, 40, 67] },
   46: { images: ['p025'], wrongAnswers: [31, 33] },
   47: { images: ['p026', 'p027', 'p089'], wrongAnswers: [76] }, //anemone
@@ -35,7 +40,7 @@ const speciesCatalog = Object.freeze({
   55: { images: ['p033', 'p034', 'p035'], wrongAnswers: [67] },
     58: { images: ['p102'], wrongAnswers: [4] }, //Dog-rose,  wrongAnswers надо
     62: {images: ['p090', 'p091'], wrongAnswers: [2] }, //apple, wrongAnswers надо
-  68: { images: ['p036'] },
+  68: { images: ['p036', 'p116', 'p117'] }, //lavender
   69: { images: ['p037'], wrongAnswers: [31, 22] },
   72: { images: ['p052'], wrongAnswers: [11] }, //лопух. wrongAnswers надо заменить
   73: { images: ['p038', 'p047', 'p092', 'p093'], wrongAnswers: [29] }, //aster, wrongAnswers надо
