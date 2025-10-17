fa8f06cd46d376698dd8809b83f72eb75c3c880a
Alisa <157376396+LorianBathory@users.noreply.github.com>
2025-10-17 10:07:56 +0200

    Add genus definitions for bellis, veronica, and hemerocallis

---
+++ src/data/genus/bellis.js
@@ -0,0 +1,17 @@
+const bellisEntries = Object.freeze({
+  119: Object.freeze({
+    names: Object.freeze({
+      ru: 'Маргаритка',
+      en: 'English daisy',
+      nl: 'Tuinmadeliefje',
+      sci: 'Bellis perennis'
+    }),
+    images: Object.freeze(['p119_1', 'p119_2'])
+  })
+});
+
+export const bellisGenus = Object.freeze({
+  id: 119,
+  slug: 'bellis',
+  entries: bellisEntries
+});
---
+++ src/data/genus/hemerocallis.js
@@ -0,0 +1,17 @@
+const hemerocallisEntries = Object.freeze({
+  36: Object.freeze({
+    names: Object.freeze({
+      ru: 'Лилейник',
+      en: 'Daylilies',
+      nl: 'Daglelie',
+      sci: 'Hemerocallis'
+    }),
+    images: Object.freeze(['p36_1', 'p36_2'])
+  })
+});
+
+export const hemerocallisGenus = Object.freeze({
+  id: 36,
+  slug: 'hemerocallis',
+  entries: hemerocallisEntries
+});
