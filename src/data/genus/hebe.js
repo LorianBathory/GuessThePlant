d9348ad7e893a437ef816136f002f8e9f1d1acbf
Alisa Sinelnikova <lorianbathory@gmail.com>
2025-10-17 13:27:37 +0200

    genus hebe - 173

--- src/data/genus/hebe.js
+++ src/data/genus/hebe.js
@@ -1,13 +1,33 @@
 const hebeEntries = Object.freeze({
   173: Object.freeze({
     names: Object.freeze({
-      ru: 'Хебе красивая',
-      en: 'Showy hebe / Shrubby veronica',
-      nl: 'Struikveronica',
-      sci: 'Hebe speciosa'
+      ru: 'Хебе',
+      en: 'Hebe',
+      nl: 'Hebe (struikveronica)',
+      sci: 'Hebe'
     }),
-    images: Object.freeze(['p173_1', 'p173_2'])
-  })
+      wrongAnswers: Object.freeze([123, 143, 172])
+  }),
+    '173_1': Object.freeze({
+        names: Object.freeze({
+            ru: 'Хебе красивая',
+            en: 'Showy hebe / Shrubby veronica',
+            nl: 'Struikveronica',
+            sci: 'Hebe speciosa'
+        }),
+        images: Object.freeze(['p173_1_1']),
+        wrongAnswers: Object.freeze([33])
+    }),
+    '47_2': Object.freeze({
+        names: Object.freeze({
+            ru: 'Хебе Андерсона',
+            en: 'Anderson\'s hebe (Shrubby veronica)',
+            nl: 'Hebe (struikveronica) Andersonii',
+            sci: 'Hebe Andersonii'
+        }),
+        images: Object.freeze(['p173_2_1']),
+        wrongAnswers: Object.freeze([33])
+    }),
 });
 
 export const hebeGenus = Object.freeze({
